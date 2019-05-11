389b3bdd50231f812e4dc532d5eb6a53e37b2fc0
Ender <xenderpugx@gmail.com>
2019-05-11 19:51:34 +0100

    Fixed dumb code

--- commands/eval.js
+++ commands/eval.js
@@ -4,19 +4,19 @@ module.exports = {
     name: 'eval',
     description: '[Developer] Evaluate',
     async execute(message, args) {
-            var SkyAdmins = [249474587530625034, 261418273009041408];
+            var SkyAdmins = ["249474587530625034", "261418273009041408"];
             if(SkyAdmins.includes(message.author.id)) return;
-              try {
-                const code = message.content.slice(6);
-                let evaled = eval(code);
-           
-                if (typeof evaled !== "string")
-                  evaled = require("util").inspect(evaled);
-           
-                message.channel.send(clean(evaled), {code:"xl"});
-              } catch (err) {
-                message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-              }
+                try {
+                  const code = args.join(" ");
+                  let evaled = eval(code);
+
+                  if (typeof evaled !== "string")
+                    evaled = require("util").inspect(evaled);
+
+                  message.channel.send(clean(evaled), {code:"xl"});
+                } catch (err) {
+                  message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+                }
             
     },
 }
